5e9aa4aceebe2fb69f9d2d3728d92c01af34941d
remyvv <remy@remysplace.de>
2014-08-14 12:52:21 +0200

    Settings refactor
    As mentioned in #17

--- classes/class-wpsws-settings.php
+++ classes/class-wpsws-settings.php
@@ -4,6 +4,8 @@ class WPSWS_Settings {
 
 	private static $instance = null;
 
+	private $setting_tabs = array();
+
 	/**
 	 * Get singleton instance of class
 	 *
@@ -31,33 +33,144 @@ class WPSWS_Settings {
 	 */
 	private function hooks() {
 		add_action( 'admin_menu', array( $this, 'add_menu_pages' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-		add_action( 'wp_ajax_wpw_save_settings', array( $this, 'save_settings' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
 	}
 
-	/**
-	 * Add menu pages
-	 */
-	public function add_menu_pages() {
-		add_menu_page( 'Overview', 'Web Services', 'manage_options', 'wpw', array( $this, 'screen_main' ) );
-	}
+	public function register_settings() {
 
-	/**
-	 * Enqueue scripts
-	 */
-	public function enqueue_scripts() {
-		if ( ! SCRIPT_DEBUG ) {
-			wp_enqueue_script( 'wpw-admin', plugin_dir_url( WPSWS_PLUGIN_FILE ) . '/assets/js/wpw-admin.js', array( 'jquery' ), '1.0.0' );
+		register_setting( 'wpsws', WP_Simple_Web_Service::OPTION_KEY );
+
+		do_action( 'wpsws_register_settings', array( &$this ) );
+
+		$db_option = WP_Simple_Web_Service::get()->get_options();
+
+		foreach( $this->setting_tabs as $tab_id => $tab ) {
+
+			foreach( $tab['sections'] as $section_id => $tab_section ) {
+				$wp_section_id = 'wpsws_'.$tab_id.'_'.$section_id;
+				add_settings_section(
+					$wp_section_id,
+					$tab_section['title'],
+					create_function( '', 'echo "<p class=\"description\">'.$tab_section['desc'].'</p>";' ),
+					'wpsws_'.$tab_id
+				);
+
+				foreach( $tab_section['settings'] as $setting_id => $setting ) {
+					$html_setting_id = WP_Simple_Web_Service::OPTION_KEY.'['.$tab_id.']['.$section_id.']['.$setting_id.']';
+					if( ! isset( $db_option[$tab_id] ) || ! isset( $db_option[$tab_id][$section_id][$setting_id] ) ) {
+						$value = '';
+					} else {
+						$value = $db_option[$tab_id][$section_id][$setting_id];
+					}
+
+
+					add_settings_field(
+						$html_setting_id,
+						$setting['label'],
+						array( $this, 'field_'.$setting['type'] ),
+						'wpsws_'.$tab_id,
+						$wp_section_id,
+						array(
+							'options' => $setting['type_options'],
+							'value' => $value,
+							'setting_id' => $html_setting_id,
+							'label' => $setting['label']
+						)
+					);
+				}
+			}
+
 		}
-		else {
-			wp_enqueue_script( 'wpw-admin', plugin_dir_url( WPSWS_PLUGIN_FILE ) . '/assets/js/wpw-admin.orig.js', array( 'jquery' ), '1.0.0' );
-		}
+
 	}
 
 	/**
-	 * Save settings via AJAX
+	 * Add a settings section to the settings screen
 	 *
+	 * @param string $id
+	 * @param string $title
+	 * @param string $desc
+	 *
+	 * @return bool
 	 */
+	public function add_tab( $id, $title='', $desc='' ) {
+		if( isset( $this->setting_tabs[$id] ) ) {
+			trigger_error( 'WP Simple Web Services: There is already a tab with the ID "'.$id.'"', E_USER_NOTICE);
+			return false;
+		} else {
+			$this->setting_tabs[$id] = array(
+				'title' => $title,
+				'desc' => $desc,
+				'sections' => array()
+			);
+			return true;
+		}
+	}
+
+	/**
+	 * Add a settings section to the settings screen
+	 *
+	 * @param string $id
+	 * @param string $title
+	 * @param string $tab_id
+	 * @param string $desc
+	 *
+	 * @return bool
+	 */
+	public function add_section( $id, $title='', $tab_id, $desc='' ) {
+		if( ! isset( $this->setting_tabs[$tab_id] ) ) {
+			trigger_error( 'WP Simple Web Services: There is no tab section with the ID "'.$tab_id.'", please register it before adding a setting to it.', E_USER_NOTICE );
+			return false;
+		} else if( isset( $this->setting_tabs[$tab_id]['sections'][$id] ) ) {
+			trigger_error( 'WP Simple Web Services: There is already a section with the ID "'.$id.'"', E_USER_NOTICE);
+			return false;
+		} else {
+			$this->setting_tabs[$tab_id]['sections'][$id] = array(
+				'title' => $title,
+				'desc' => $desc,
+				'settings' => array()
+			);
+			return true;
+		}
+	}
+
+	/**
+	 * Add setttings field to a settings section on the setting screen
+	 *
+	 * @param string $id
+	 * @param string $label
+	 * @param string $type
+	 * @param string $tab_id
+	 * @param string $section_id
+	 * @param string $desc
+	 * @param array $type_options
+	 *
+	 * @return bool
+	 */
+	public function add_setting( $id, $label='', $type='text', $tab_id, $section_id, $desc='', $type_options=array() ) {
+
+		if( ! isset( $this->setting_tabs[$tab_id] ) ) {
+			trigger_error( 'WP Simple Web Services: There is no tab section with the ID "'.$tab_id.'", please register it before adding a setting to it.', E_USER_NOTICE );
+			return false;
+		} else if( ! isset( $this->setting_tabs[$tab_id]['sections'][$section_id] ) ) {
+			trigger_error( 'WP Simple Web Services: There is no settings section with the ID "'.$section_id.'", please register it before adding a setting to it.', E_USER_NOTICE );
+			return false;
+		} else if( isset( $this->setting_tabs[$tab_id]['sections'][$section_id]['settings'][$id] ) ) {
+			trigger_error( 'WP Simple Web Services: There is already a setting with the ID "'.$id.'" in section "'.$section_id.'"', E_USER_NOTICE);
+			return false;
+		} else {
+			$this->setting_tabs[$tab_id]['sections'][$section_id]['settings'][$id] = array(
+				'id' => $id,
+				'label' => $label,
+				'desc' => $desc,
+				'type' => $type,
+				'type_options' => $type_options,
+				'section_id' => $section_id,
+			);
+			return true;
+		}
+	}
+
 	public function save_settings() {
 
 		// Security check
@@ -90,16 +203,101 @@ class WPSWS_Settings {
 	}
 
 	/**
+	 * Add menu pages
+	 */
+	public function add_menu_pages() {
+		add_menu_page( 'Overview', 'Web Services', 'manage_options', 'wpsws', array( $this, 'screen_main' ) );
+	}
+
+	public function field_checkbox($args) {
+	?>
+		<label><input type="checkbox" name="<?php echo $args['setting_id']; ?>" <?php checked( 'true', $args['value'] ); ?> value="true" /><?php echo $args['label']; ?></label>
+	<?php
+	}
+
+	public function field_checkbox_array($args) {
+		if( '' === $args['value'] ) {
+			$args['value'] = array();
+		}
+		foreach( $args['options']['fields'] as $field_id => $field_title ) {
+		?>
+			<label><input type="checkbox" name="<?php echo $args['setting_id']; ?>[]" <?php checked( in_array( $field_id, $args['value'] ) ); ?> value="<?php echo $field_id; ?>" /><?php echo $field_title; ?></label><br/>
+		<?php
+		}
+	}
+
+	public function field_radio_array($args) {
+		foreach( $args['options']['fields'] as $field_id => $field_title ) {
+			?>
+			<label><input type="radio" name="<?php echo $args['setting_id']; ?>" <?php checked( $args['value'], $field_id ); ?> value="<?php echo $field_id; ?>" /><?php echo $field_title; ?></label><br/>
+		<?php
+		}
+	}
+
+	public function field_text($args) {
+
+		$disabled = ( isset( $args['options']['disabled'] ) && $args['options']['disabled'] );
+		if( $disabled ) {
+			$value = $args['options']['value'];
+		} else {
+			$value = $args['value'];
+		}
+	?>
+		<input style="width:100%;" type="text" <?php if( !$disabled ){ ?>name="<?php echo $args['setting_id']; ?>" <?php } else{ ?>disabled="disabled"<?php } ?> value="<?php echo $value; ?>" />
+	<?php
+	}
+
+	/**
 	 * The main screen
 	 */
 	public function screen_main() {
-		?>
+	?>
 		<div class="wrap" id="wpw-wrap">
 			<h2>WordPress Simple Web Services</h2>
 
 			<div>
-				<p><?php _e( 'Welcome to WordPress Simple Web Services! You can enable post types by clicking the "Enable post type" checkbox below. If you encounter problems or find any bugs please report hem on <a href="https://github.com/barrykooij/wp-simple-web-services/issues" target="_blank">GitHub</a>.', 'wpsws' ); ?></p>
-				<?php do_action( 'wpsws_general_settings' ); ?>
+				<p><?php _e( 'Welcome to WordPress Simple Web Services! You can enable post types by clicking the "Enable post type" checkbox below. If you encounter problems or find any bugs please report them on <a href="https://github.com/barrykooij/wp-simple-web-services/issues" target="_blank">GitHub</a>.', 'wpsws' ); ?></p>
+				<?php settings_errors(); ?>
+					<h2 class="nav-tab-wrapper hide-if-no-js">
+						<?php foreach( $this->setting_tabs as $tab_id => $tab ) { ?>
+							<a href="#" data-tab_id="<?php echo $tab_id; ?>" class="nav-tab"><?php echo $tab['title']; ?></a>
+						<?php } ?>
+					</h2>
+				<form method='post' action='<?php echo admin_url('options.php'); ?>'>
+				<?php
+					settings_fields( 'wpsws' );
+					foreach( $this->setting_tabs as $tab_id => $tab ) {
+						echo '<div id="'.$tab_id.'" class="wpsws_tab">';
+						echo '<h2>'.$tab['title'].'</h2>';
+						echo '<p class="description">'.$tab['desc'].'</p>';
+
+						do_settings_sections( 'wpsws_'.$tab_id );
+						submit_button( __( 'Save', 'wpw' ) );
+						echo '</div>';
+					}
+				?>
+				</form>
+				<script>
+					jQuery(document).ready(function($) {
+						var tabs = $('.wpsws_tab');
+						var nav_items = $('.nav-tab');
+						tabs.hide();
+						tabs.first().show();
+						nav_items.first().addClass('nav-tab-active');
+						nav_items.click(function(e){
+							e.preventDefault();
+							nav_items.removeClass('nav-tab-active');
+							tabs.hide();
+							tabs.parent().find('#'+$(this).data('tab_id')).show();
+							$(this).addClass('nav-tab-active');
+						})
+					});
+				</script>
+				<?php
+					if( null !== do_action( 'wpsws_general_settings' ) ) {
+						trigger_error( 'WP Simple Webservices: the use of the hook "wpsws_general_settings" is deprecated and will be removed in a future version', E_USER_NOTICE );
+					}
+				?>
 			</div>
 		</div>
 	<?php
--- classes/class-wpsws-webservice-get-posts.php
+++ classes/class-wpsws-webservice-get-posts.php
@@ -34,6 +34,98 @@ class WPSWS_Webservice_get_posts {
 	private function hooks() {
 		add_action( 'wpsws_webservice_get_posts', array( $this, 'get_posts' ) );
 		add_action( 'wpsws_general_settings', array( $this, 'settings' ), 1 );
+
+		add_action( 'wpsws_register_settings', array( $this, 'register_settings' ), 2 );
+	}
+
+	/**
+	 * Register all get_posts settings
+	 *
+	 * @param WPSWS_Settings $settings
+	 *
+	 */
+	public function register_settings( $settings ) {
+		$settings->add_tab( 'get_posts', __( 'Post Types', 'wpw' ), __( 'You can enable post types by clicking the "Enable post type" checkbox below.', 'wpw' ) );
+
+		$post_types = get_post_types( array( 'public' => true ), 'objects' );
+
+		$nice_urls = false;
+		$permalink_structure = get_option('permalink_structure');
+		if( '/%postname%/' === $permalink_structure ) {
+			$nice_urls = true;
+		}
+
+		if ( count( $post_types ) > 0 ) {
+
+			foreach ( $post_types as $key => $post_type ) {
+				$settings->add_section( $key, $post_type->labels->name, 'get_posts' );
+
+				$settings->add_setting( 'enabled', 'Enable post type', 'checkbox', 'get_posts', $key );
+
+
+				if( $nice_urls ) {
+					$webservice_url = get_site_url() . '/webservice/get_posts/?post_type=' . $key;
+				} else {
+					$webservice_url = get_site_url() . '?webservice=1&service=get_posts&post_type=' . $key;
+				}
+
+				$settings->add_setting( 'webservice_url', 'Webservice URL', 'text', 'get_posts', $key, '', array( 'disabled' => true, 'value' => $webservice_url ) );
+
+
+				// Default post type fields
+				$post_type_supports = array(
+					'ID'            => 'ID',
+					'post_date'     => __( 'Post Date', 'wpw' ),
+					'post_status'   => __( 'Post Status', 'wpw' ),
+					'post_modified' => __( 'Post Modified', 'wpw' ),
+					'post_parent'   => __( 'Post Parent', 'wpw' ),
+					'menu_order'    => __( 'Menu Order', 'wpw' ),
+					'post_type'     => __( 'Post Type', 'wpw' )
+				);
+
+				// Default post type fields that might be supported
+				$post_type_maybe_supports = array(
+					'title'     => array( array( 'key' => 'post_title', 'value' => __( 'Post Title', 'wpw' ) ) ),
+					'editor'    => array( array( 'key' => 'post_content', 'value' => __( 'Post Content', 'wpw' ) ) ),
+					'author'    => array( array( 'key' => 'post_author', 'value' => __( 'Post Author', 'wpw' ) ) ),
+					'thumbnail' => array( array( 'key' => 'thumbnail', 'value' => __( 'Thumbnail', 'wpw' ) ) ),
+					'excerpt'   => array( array( 'key' => 'post_excerpt', 'value' => __( 'Post Excerpt', 'wpw' ) ) ),
+					'comments'  => array( array( 'key' => 'comment_status', 'value' => __( 'Comment Status', 'wpw' ) ), array( 'key' => 'comment_count', 'value' => __( 'Comment Count', 'wpw' ) ) ),
+				);
+
+				// Check if the current post type supports the optional fields
+				foreach ( $post_type_maybe_supports as $supports_key => $post_type_maybe_support_fields ) {
+					if ( post_type_supports( $key, $supports_key ) ) {
+						foreach ( $post_type_maybe_support_fields as $post_type_maybe_support_field ) {
+							$post_type_supports[$post_type_maybe_support_field['key']] = $post_type_maybe_support_field['value'];
+						}
+					}
+				}
+				$settings->add_setting( 'fields', 'Enable Fields', 'checkbox_array', 'get_posts', $key, '', array( 'fields' => $post_type_supports ) );
+
+
+				$custom_fields = array();
+				global $wpdb;
+				$query = "SELECT DISTINCT({$wpdb->postmeta}.meta_key)
+        						FROM {$wpdb->posts}
+        						INNER JOIN {$wpdb->postmeta}
+        						ON {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
+        						WHERE {$wpdb->posts}.post_type = '%s'
+        						AND {$wpdb->postmeta}.meta_key != ''
+        						ORDER BY {$wpdb->postmeta}.meta_key ASC";
+				$db_meta_keys = $wpdb->get_col($wpdb->prepare($query, $key));
+				if ( is_array( $db_meta_keys ) && count( $db_meta_keys ) > 0 ) {
+					foreach ( $db_meta_keys as $custom_field ) {
+						if ( substr( $custom_field, 0, 1 ) != '_' ) {
+							$custom_fields[$custom_field] = $custom_field;
+						}
+					}
+				}
+				$settings->add_setting( 'custom', 'Custom Fields', 'checkbox_array', 'get_posts', $key, '', array( 'fields' => $custom_fields ) );
+
+			}
+
+		}
 	}
 
 	/**
